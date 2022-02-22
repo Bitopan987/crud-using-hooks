194e4a22ef11372211b1ae7086b06192ebb02f97
bitopan987 <bitopanxon1@gmail.com>
2022-02-22 19:21:08 +0530

    refactor

---
+++ src/Components/User.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+function User({ user, index, removeUser }) {
+  return (
+    <li className="flex w-1/2 justify-between items-center border border-dashed mt-3 p-3 bg-blue-300">
+      <div>
+        <h3 className="capitalize text-2xl text-gray-700">{user.name}</h3>
+        <p className="text-sm text-gray-600">{user.username}</p>
+      </div>
+      <Link
+        to={{
+          pathname: `/edit/${user.username}`,
+          id: index,
+        }}
+      >
+        <button>Edit</button>
+      </Link>
+      <span
+        onClick={() => removeUser(index)}
+        className="text-red-500 cursor-pointer hover:text-red-900 font-bold"
+      >
+        X
+      </span>
+    </li>
+  );
+}
+
+export default User;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './Components/style/style.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import EditUser from './Components/EditUser';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/" exact>
+        <App />
+      </Route>
+      <Route path="/edit/:id" component={EditUser} />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
